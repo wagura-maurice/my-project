3b66cbf1fd16753c5c97ee265fb1060033bc4726
Wagura Maurice <business@waguramaurice.com>
2023-09-25 16:35:57 +0300

    update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
-// https://vitejs.dev/config/
+// Define a function to resolve the path to the 'src' directory
+function resolveSrc(_path) {
+  return path.resolve(__dirname, _path);
+}
+
 export default defineConfig({
   plugins: [vue()],
-})
+  resolve: {
+    alias: {
+      '@': resolveSrc('src'), // Use the resolveSrc function to specify the root 'src' directory
+    },
+  },
+});
