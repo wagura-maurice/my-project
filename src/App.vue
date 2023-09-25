eed9eeb1a6a628dcc35b7108f7af2c15dbb69dde
Wagura Maurice <business@waguramaurice.com>
2023-09-25 16:35:22 +0300

    update App.vue, main.ts and style.css

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,21 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
+<!-- App.vue -->
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <WelcomeNavbar />
+  <div class="mt-16">
+    <!-- Include your navigation bar component here -->
+    <router-view></router-view> <!-- Renders the content of the current route -->
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <WelcomeFooter />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script>
+import WelcomeNavbar from './components/common/WelcomeNavbar.vue';
+import WelcomeFooter from './components/common/WelcomeFooter.vue';
+
+export default {
+  components: {
+    WelcomeNavbar,
+    WelcomeFooter,
+  },
+};
+</script>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,13 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
+import { useStore, key } from '@/store';
+// import { useStore, key } from './store/index.ts'; // Import your Vuex store
+import router from './router/index.ts'; // Import your Vue Router
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(useStore, key); // Use the Vuex store
+app.use(router); // Use the Vue Router
+
+app.mount('#app');
