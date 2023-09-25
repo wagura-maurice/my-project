03a0d41924b4299d7dfd3860ddcdd27111248cfe
Wagura Maurice <business@waguramaurice.com>
2023-09-25 16:35:26 +0300

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
 import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
-import { useStore, key } from '@/store';
-// import { useStore, key } from './store/index.ts'; // Import your Vuex store
+import { useStore, key } from './store/index.ts'; // Import your Vuex store
 import router from './router/index.ts'; // Import your Vue Router
 
 const app = createApp(App);
