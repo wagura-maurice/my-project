5703067b5c61f8b42786991853bc03409e5220c9
Wagura Maurice <business@waguramaurice.com>
2023-09-25 16:34:47 +0300

    feat: create Dashboard.vue

---
+++ src/views/backend/Dashboard.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class="min-h-screen bg-gray-100">
+      <!-- Main content container -->
+      <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+        <div class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+          <!-- Widget 1 - Example Card -->
+          <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="p-6">
+              <div class="flex items-center">
+                <div class="ml-4 text-lg leading-7 font-semibold">Widget 1</div>
+              </div>
+              <!-- Widget content goes here -->
+            </div>
+          </div>
+  
+          <!-- Widget 2 - Example Card -->
+          <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="p-6">
+              <div class="flex items-center">
+                <div class="ml-4 text-lg leading-7 font-semibold">Widget 2</div>
+              </div>
+              <!-- Widget content goes here -->
+            </div>
+          </div>
+  
+          <!-- Widget 3 - Example Card -->
+          <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="p-6">
+              <div class="flex items-center">
+                <div class="ml-4 text-lg leading-7 font-semibold">Widget 3</div>
+              </div>
+              <!-- Widget content goes here -->
+            </div>
+          </div>
+  
+          <!-- Add more widgets as needed -->
+  
+        </div>
+  
+        <!-- Statistics Section - Example -->
+        <div class="mt-8">
+          <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="p-6">
+              <div class="flex items-center">
+                <div class="ml-4 text-lg leading-7 font-semibold">Statistics</div>
+              </div>
+              <!-- Statistics content goes here -->
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script lang="ts">
+  import { defineComponent } from 'vue';
+  
+  export default defineComponent({
+    setup() {
+      // Your data and logic can be added here
+  
+      return {};
+    },
+  });
+  </script>
+  
+  <!-- If you want to add style scoped to this component -->
+  <style scoped>
+  </style>
+  
